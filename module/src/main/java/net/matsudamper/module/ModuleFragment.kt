8db8b96f11ef4ee54e80c4332c3a048152f3f850
matsudamper <let@matsudamper.net>
2022-04-12 00:30:24 +0900

    fix name

--- module/src/main/java/net/matsudamper/module/ModuleFragment.kt
+++ module/src/main/java/net/matsudamper/module/ModuleFragment.kt
@@ -11,7 +11,7 @@ import net.matsudamper.module.databinding.FragmentFirstBinding
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ModuleFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
