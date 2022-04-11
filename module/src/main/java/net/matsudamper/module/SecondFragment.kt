4f86e35f0e8735acdaf957565e2c4e81f9f88aa9
matsudamper <let@matsudamper.net>
2022-04-12 00:16:07 +0900

    add error

--- module/src/main/java/net/matsudamper/module/SecondFragment.kt
+++ module/src/main/java/net/matsudamper/module/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
 import androidx.navigation.fragment.findNavController
 import net.matsudamper.module.databinding.FragmentSecondBinding
 
