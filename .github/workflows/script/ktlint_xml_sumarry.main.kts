11c211cf313c0ca07d148d68c27da5507afd2b61
matsudamper <let@matsudamper.net>
2022-04-13 10:25:32 +0900

    tmp

---
+++ .github/workflows/script/ktlint_xml_sumarry.main.kts
@@ -0,0 +1,18 @@
+#!/usr/bin/env kotlin
+@file:CompilerOptions("-opt-in=kotlin.RequiresOptIn", "-nowarn")
+
+import java.io.File
+
+val filename = args[0]
+
+val result = """
+KTLINT_SUMMARY<<EOF
+<details>
+<summary>retult.xml</summary>
+
+```xml
+${File(filename).readText()}
+```
+EOF
+""".trimIndent()
+println(result)
